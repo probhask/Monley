0432b4498d0d04146159ecbe3c8d9457dc03d756
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2024-08-04 11:27:14 +0530

    add render server retrigger

--- client/src/store/slice/bannerSlice.ts
+++ client/src/store/slice/bannerSlice.ts
@@ -2,45 +2,51 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Banner } from "../../types";
 
-
-export const getBanner = createAsyncThunk('getBanner', async () => {
-    // console.log("env",import.meta.env.VITE_BACKEND_URL);
-    try {
-        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/banner`);
+export const getBanner = createAsyncThunk("getBanner", async () => {
+  // console.log("env",import.meta.env.VITE_BACKEND_URL);
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/banner`
+    );
     const { data } = response;
     // console.log("banner", data.banner);
-    return data.banner
-    } catch (error) {
-        throw new Error(`getbanner failed ${error}`);
-    }
+    return data.banner;
+  } catch (error) {
+    throw new Error(`getbanner failed ${error}`);
+  }
 });
 
 type BannerInitailState = {
-    data: Banner|null,
-    isLoading: boolean,
-    isError: string|undefined,
-}
+  data: Banner | null;
+  isLoading: boolean;
+  isError: string | undefined;
+};
 
 const initialState: BannerInitailState = {
-    data:null,isLoading: false,isError: '',
-}
+  data: null,
+  isLoading: false,
+  isError: "",
+};
 
 const bannerSlice = createSlice({
-    name: "banner",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(getBanner.pending, (state) => {
-            state.isLoading = true;
-            state.isError = '';
-        }).addCase(getBanner.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isError = '';
-            state.data = action.payload[0];
-        }).addCase(getBanner.rejected, (state, action) => {
-            state.isLoading = false;
-            state.isError = action.error.message;
-        })
-    }
-})
+  name: "banner",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBanner.pending, (state) => {
+        state.isLoading = true;
+        state.isError = "";
+      })
+      .addCase(getBanner.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = "";
+        state.data = action.payload[0];
+      })
+      .addCase(getBanner.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = action.error.message;
+      });
+  },
+});
 export default bannerSlice.reducer;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cron = require("node-cron");
+const shell = require("shelljs");
+const https = require("https");
+
 const itemRouter = require("./router/itemsRouter");
 const bannerRouter = require("./router/bannerRouter");
 const searchRouter = require("./router/searchRouter");
@@ -8,11 +12,30 @@ const customerRouter = require("./router/customerRouter");
 const cartRouter = require("./router/cartRouter");
 const orderRouter = require("./router/orderRouter");
 
+const backendUrl = "https://monley.onrender.com";
+
 const app = express();
 
 app.use(bodyParser.json());
 
-// app.use(cors(corsOptiions));
+cron.schedule("*/14 * * * * ", () => {
+  console.log("restarting server");
+
+  https
+    .get(backendUrl, (res) => {
+      if (res.statusCode === 200) {
+        console.log("server restarted");
+      } else {
+        console.error(
+          "failed to restart server with status code",
+          res.statusCode
+        );
+      }
+    })
+    .on("error", (err) => {
+      console.error("error during restart: ", err.message);
+    });
+});
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
@@ -22,8 +45,10 @@ app.use(bodyParser.json());
 //   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 //   next();
 // });
+
 const corsOptions = {
   origin: "https://monley.netlify.app",
+  // origin: "http://http://localhost:5173/",
   methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "HEAD"],
   credentials: true,
 };
